78fa9cc78117032f1fd98bbd7c0f73a8191fbf67
clintonjavery <clintonjavery@gmail.com>
2022-12-07 15:01:51 -0600

    Add project files.

---
+++ FileChopper/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FileChopper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
