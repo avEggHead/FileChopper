33c7c7a02d30c8de9830c9be8fc0ce93734bc333
clintonjavery <clintonjavery@gmail.com>
2022-12-21 07:11:19 -0600

    testing file size

--- FileChopper/Program.cs
+++ FileChopper/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Testing file size saving");
+            string testString = "This string has a certain length asdfasdfa safsdf asdf asdf ";
+            Console.WriteLine("Length of string is: " + testString.Length);
+            var path = @"C:\TestLength.txt";
+            File.Create(path).Close();
+            File.AppendAllText(path, testString);
+            Console.WriteLine("I just wrote the file go see if it is " + testString.Length + " bytes");
+            Console.ReadKey();
         }
     }
 }
